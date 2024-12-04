7ff469e335c46a1d8a2943e43aaa686226334dfe
magicjuandi <jdgraoorms@gmail.com>
2024-12-03 21:13:25 -0500

    check

--- src/test/java/com/cue/GestordeTareas/GestordeTareasApplicationTests.java
+++ src/test/java/com/cue/GestordeTareas/GestordeTareasApplicationTests.java
@@ -1,15 +1,22 @@
 package com.cue.GestordeTareas;
 
 import com.cue.GestordeTareas.domain.entities.Task;
+import com.cue.GestordeTareas.repositories.TaskRepository;
 import com.cue.GestordeTareas.services.TaskService;
+import com.cue.GestordeTareas.services.impl.TaskServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,7 +32,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,6 +46,10 @@ class GestordeTareasApplicationTests {
 	private TaskService taskService;
 	@Autowired
 	private MockMvc mockMvc;
+	@Mock
+	private TaskRepository taskRepositoryU;
+	@InjectMocks
+	private TaskServiceImpl taskServiceU;
 	private final Task task = new Task.Builder()
 			.id(1)
 			.title("Titulo de tarea")
@@ -97,6 +107,39 @@ class GestordeTareasApplicationTests {
 						.content(new ObjectMapper().writeValueAsString(task)))
 				.andExpect(status().isOk());
 	}
-
+	@Test
+	public void testSaveTask(){
+		Task task = new Task(1,"New Task");
+		when(taskRepositoryU.save(any())).thenReturn(task);
+		Task result = taskServiceU.saveTask(task);
+		assertNotNull(result);
+		verify(taskRepositoryU,times(1)).save(any());
+	}
+	@Test
+	public void testUpdateTask(){
+		Task task = new Task(1,"New Task");
+		when(taskRepositoryU.findById(anyInt())).thenReturn(Optional.of(task));
+		when(taskRepositoryU.save(any())).thenReturn(task);
+		Task result = taskServiceU.updateTask(task.getId(),"New Task Title");
+		assertNotNull(result);
+		verify(taskRepositoryU,times(1)).save(any());
+	}
+	@Test
+	public void testFindTask(){
+		Task task = new Task(1,"New Task");
+		when(taskRepositoryU.findById(1)).thenReturn(Optional.of(task));
+		Task result = taskServiceU.getTask(task.getId()).orElseThrow();
+		assertNotNull(result);
+		verify(taskRepositoryU,times(1)).findById(task.getId());
+	}
+	@Test
+	public void testDeleteTask(){
+		int taskId = 1;
+		when(taskRepositoryU.findById(taskId)).thenReturn(Optional.of(new Task(taskId,"Existing Task")));
+		doNothing().when(taskRepositoryU).deleteById(taskId);
+		String result = taskServiceU.deleteTask(taskId);
+		assertEquals("Task Deleted", result);
+		verify(taskRepositoryU,times(1)).deleteById(taskId);
+	}
 
 }
